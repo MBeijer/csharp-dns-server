a62fa0f0954bde735f71614eae14d8831088ae18
Marlon Beijer <marlon@eevul.org>
2025-10-11 00:43:16 +0200

    Update packages + cleanup

--- Dns.Cli/Startup.cs
+++ Dns.Cli/Startup.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Linq;
 using System.Text;
 using System.Text.Json;
-using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 using System.Text.Json.Serialization.Metadata;
 using System.Xml;
